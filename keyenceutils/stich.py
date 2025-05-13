8353ad5a70a8e4f237605ac7f6cd957057b1e069
Bhanu Sharma <bsharma810@gmail.com>
2025-05-13 09:25:39 +0900

    Update stich.py

--- keyenceutils/stich.py
+++ keyenceutils/stich.py
@@ -20,18 +20,52 @@ class ImageMetadata:
     Methods:
         __init__(tif):
             Initializes the ImageMetadata object by extracting and parsing XML data from the given TIFF file.
+
+<Data>
+            
+<SavingImageSize Type="Keyence.Micro.Bio.Common.Utility.KeyValueContainer.SizeKeyValueContainer, Keyence.Micro.Bio.Common.Utility.KeyValueContainer, Version=1.1.2.14, Culture=neutral, PublicKeyToken=null">
+<Width Type="System.Int32">480</Width>
+<Height Type="System.Int32">360</Height>
+</SavingImageSize>
+
+
+<Lens Type="Keyence.Micro.Bio.Common.Data.Metadata.Conditions.LensCondition, Keyence.Micro.Bio.Common.Data.Metadata, Version=1.1.2.14, Culture=neutral, PublicKeyToken=null">
+<LensName Type="System.String">PlanApo 4x 0.20/20.00mm :Default</LensName>
+<RevolverPosition Type="System.Int32">0</RevolverPosition>
+</Lens>
+
+
+<XyStageRegion Type="Keyence.Micro.Bio.Common.Utility.KeyValueContainer.RectangleKeyValueContainer, Keyence.Micro.Bio.Common.Utility.KeyValueContainer, Version=1.1.2.14, Culture=neutral, PublicKeyToken=null">
+<X Type="System.Int32">28177000</X>
+<Y Type="System.Int32">59189000</Y>
+<Width Type="System.Int32">3623441</Width>
+<Height Type="System.Int32">2717581</Height>
+</XyStageRegion>
+
+
+<ExposureTime Type="Keyence.Micro.Bio.Common.Utility.KeyValueContainer.ExposureTimeKeyValueContainer, Keyence.Micro.Bio.Common.Utility.KeyValueContainer, Version=1.1.2.14, Culture=neutral, PublicKeyToken=null">
+<Numerator Type="System.Int32">1</Numerator>
+<Denominator Type="System.Int32">30</Denominator>
+<Line Type="System.Int32">761</Line>
+</ExposureTime>
+
+</Data>
+
     """
 
     def __init__(self, tif: str):
         self.__xml_file = tif.replace('.tif', '.xml')
-        self.image_positions: tuple[float] = None
-        self.dimensions: tuple[int] = None
-        self.nm_per_pixel_values: float = None
-        self.lens_name = None
+        self.image_positions: tuple[float] = None       # initialize image position
+        self.dimensions: tuple[int] = None              # initialize image dimensions
+        self.nm_per_pixel_values: float = None          # initialize pixel size in nm
+        self.lens_name = None                           # initialize lenz name
+        self.exposure_time = None                        # initialize exp time
+
         # Read TIFF file as binary
         with open(tif, "rb") as file:
             content = file.read().decode(errors="ignore")   # decode as string
         print(content)
+
         # Extract XML content from TIFF file (file starts with <Data> and ends with <\Data>)
         match = re.search(r"<Data>.*?</Data>", content, re.DOTALL)
 
@@ -39,12 +73,13 @@ class ImageMetadata:
             raise ValueError(f"Could not extract XML data from {tif}")
         xml_content = match.group(0)  # extract matched xml content
 
+
         if SAVE_XML:
             with open(self.__xml_file, "w", encoding="utf-8") as xml_out:
                 xml_out.write(xml_content)
 
         else:
-            print(f"XML not saved for {self._xml_file}")
+            print(f"XML not saved for {self.__xml_file}")
 
         # Parse the XML content directly and extract coordinates and dimensions
         tree = ET.ElementTree(ET.fromstring(xml_content))
@@ -52,37 +87,29 @@ class ImageMetadata:
 
         if region is None:
             raise ValueError(f"File: {self.__xml_file} | Attributes not found")
+       
         # Extract X and Y coordinates
-        self.image_positions = (
-            int(region.find('X').text), int(region.find('Y').text))
+        self.image_positions = (int(region.find('X').text), int(region.find('Y').text))
 
         # Extract width and height from the SavingImageSize section
-        self.dimensions = (
-            int(tree.find('.//SavingImageSize/Width').text),
-            int(tree.find('.//SavingImageSize/Height').text)
-        )
-
+        self.dimensions = ( int(tree.find('.//SavingImageSize/Width').text),
+                            int(tree.find('.//SavingImageSize/Height').text)
+                 )
         # Extract width from XyStageRegion and SavingImageSize to calculate nm_per_pixel
         # Width in nm from XyStageRegion
         # Width in pixels from SavingImageSize
-        self.nm_per_pixel_values = int(region.find(
-            'Width').text) / self.dimensions[0]  # Conversion factor
+        self.nm_per_pixel_values = int(region.find('Width').text) / self.dimensions[0]  # Conversion factor
+        
 
         # Extract LensName
-        # <Lens Type="Keyence.Micro.Bio.Common.Data.Metadata.Conditions.LensCondition, Keyence.Micro.Bio.Common.Data.Metadata, Version=1.1.2.14, Culture=neutral, PublicKeyToken=null">
-        # <LensName Type="System.String">PlanApo 4x 0.20/20.00mm :Default</LensName>
         lens_name = tree.find('.//LensName')
         if lens_name is not None:
             self.lens_name = lens_name.text
         else:
             self.lens_name = "LensName not found"
-        '''
-        # Extract ExposureTime from the XML file
-        # <ExposureTime Type="Keyence.Micro.Bio.Common.Utility.KeyValueContainer.ExposureTimeKeyValueContainer, Keyence.Micro.Bio.Common.Utility.KeyValueContainer, Version=1.1.2.14, Culture=neutral, PublicKeyToken=null">
-        #<Numerator Type="System.Int32">1</Numerator>
-        #<Denominator Type="System.Int32">30</Denominator>
-        #<Line Type="System.Int32">761</Line>
-         #</ExposureTime>
+
+        
+        # Extract ExposureTime from the XML file        
         exposure_time = tree.find('.//ExposureTime')
         if exposure_time is not None:
             numerator = exposure_time.find('Numerator')
@@ -90,7 +117,7 @@ class ImageMetadata:
             if numerator is not None and denominator is not None:
                 self.exposure_time = float(numerator.text) / float(denominator.text)
             else:
-                self.exposure_time = None     '''
+                self.exposure_time = None     
 
     def __str__(self):
         return f"X: {self.image_positions[0]}, Y: {self.image_positions[1]}, Width: {self.dimensions[0]}, Height: {self.dimensions[1]}, nm_per_pixel: {self.nm_per_pixel_values}, lens_name: {self.lens_name}, Exposure_Time: {self.exposure_time}"
@@ -101,8 +128,8 @@ class ImageMetadata:
             "Y": int(self.image_positions[1]/self.nm_per_pixel_values),
             "W": self.dimensions[0],
             "H": self.dimensions[1],
-            "LensName": self.lens_name
-           # "ExposureTime": self.exposure_time
+            "LensName": self.lens_name,
+            "ExposureTime": self.exposure_time
         }
 
 
